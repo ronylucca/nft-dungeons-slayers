5bdb18e7a80c58d368a6ed1c508d6a71beb6a332
Rony de Lucca <rony.oliveira@altran-consultoria.com.br>
2022-01-19 18:01:06 -0300

    Check user NFT flow

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import SelectCharacter from './Components/SelectCharacter';
 import twitterLogo from './assets/twitter-logo.svg';
+import {CONTRACT_ADDRESS, transformCharacterData} from './constants';
+import myEpicGame from './utils/MyEpicGame.json';
+import {ethers} from 'ethers';
 
 // Constants
 const TWITTER_HANDLE = '_dungeonslayer';
@@ -50,6 +53,16 @@ const App = () => {
       console.log(error);
     }
   };
+
+  const checkNetwork = async () => {
+    try { 
+      if (window.ethereum.networkVersion !== '4') {
+        alert("Please connect to Rinkeby!")
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
 
   //Render Methods
   const renderContent = () => {
@@ -111,7 +124,42 @@ const App = () => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    checkNetwork();
   }, []);
+
+  //Contract interations UseEffect
+  useEffect(() => {
+  /*
+   * The function we will call that interacts with out smart contract
+   */
+  const fetchNFTMetadata = async () => {
+    console.log('Checking for Character NFT on address:', currentAccount);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const gameContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      myEpicGame.abi,
+      signer
+    );
+
+    const txn = await gameContract.checkIfUserHasNFT();
+    if (txn.name) {
+      console.log('User has character NFT');
+      setCharacterNFT(transformCharacterData(txn));
+    } else {
+      console.log('No character NFT found');
+    }
+  };
+
+  /*
+   * We only want to run this, if we have a connected wallet
+   */
+  if (currentAccount) {
+    console.log('CurrentAccount:', currentAccount);
+    fetchNFTMetadata();
+  }
+}, [currentAccount]);
 
   return (
     <div className="App">
